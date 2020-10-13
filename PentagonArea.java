7f48f0c0a8781f7e8075dad82a56ae6507ab0f70
鈴木ちほり <suzukichihori@suzukichihoris-MacBook-Air.local>
2020-10-13 15:28:54 -0700

    second commit

--- PentagonArea.java
+++ PentagonArea.java
@@ -1,5 +1,6 @@
 package week2.day1;
 import java.util.Scanner;
+import java.util.*;
 
 public class PentagonArea {
     public static void main(String[] args) {
@@ -7,17 +8,32 @@ public class PentagonArea {
         System.out.print("Enter the first city: ");
         Scanner input1 = new Scanner(System.in);
         String city1 = input1.nextLine();
+        city1 = city1.substring(0,1).toUpperCase() + city1.substring(1).toLowerCase();
 
         System.out.print("Enter the second city: ");
         Scanner input2 = new Scanner(System.in);
         String city2 = input2.nextLine();
+        city2 = city2.substring(0,1).toUpperCase() + city2.substring(1).toLowerCase();
 
         System.out.print("Enter the third city: ");
         Scanner input3 = new Scanner(System.in);
         String city3 = input3.nextLine();
+        city3 = city3.substring(0,1).toUpperCase() + city3.substring(1).toLowerCase();
         
-        System.out.println("The three cities in alphabetical order are " + city1 + " " + city2 + " " + city3);
+        ArrayList<String> cities = new ArrayList<>();
+        cities.add(city1);
+        cities.add(city2);
+        cities.add(city3);
 
+        Collections.sort(cities);
+        String ans = String.join(" " ,cities);
+
+        // String ans = "";
+        // for(int i = 0; i < cities.size(); i++){
+        //     ans = ans + " " + cities.get(i);
+        // }
+
+        System.out.println("The three cities in alphabetical order are " + ans);
 
     }
 }
